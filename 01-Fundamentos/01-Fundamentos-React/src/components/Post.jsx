b16f1bc8ec0a82c366a1c852c36d10bf348ff00f
Rebeca Yun <rebecajhyun@gmail.com>
2024-03-30 12:37:13 -0300

    post component

---
+++ 01-Fundamentos/01-Fundamentos-React/src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img className={styles.avatar} src="https://github.com/rebecayun.png" />
+            <div className={styles.authorInfo}>
+              <strong>Rebeca Yun</strong>
+              <span>Web Developer</span>
+            </div>
+          </div>
+
+          <time title='30 de Março às 11:12h' dateTime='2024-03-30 11:12'>Publicado há 1h</time>
+        </header>
+
+        <div className={styles.content}>
+          <p>Fala galeraa 👋 </p>
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
+          <p>👉<a href=""> jane.design/doctorcare </a></p>
+          <p>
+            <a href="">#novoprojeto </a>
+            <a href="">#nlw </a>
+            <a href="">#rocketseat </a>
+          </p>
+
+        </div>
+      </article>
+    );
+}
