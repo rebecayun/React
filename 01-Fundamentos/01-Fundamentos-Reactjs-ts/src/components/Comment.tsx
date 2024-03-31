ee26f2f24803de30e860ff88eca463ce3da5107e
Rebeca Yun <rebecajhyun@gmail.com>
2024-03-31 18:25:29 -0300

    project complete

--- 01-Fundamentos/01-Fundamentos-Reactjs-ts/src/components/Comment.tsx
+++ 01-Fundamentos/01-Fundamentos-Reactjs-ts/src/components/Comment.tsx
@@ -23,7 +23,11 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/rebecayun.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/rebecayun.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
