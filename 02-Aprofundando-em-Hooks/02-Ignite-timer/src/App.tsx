934be416ff17d9eb7a7e57263298a1f4b62714b5
Rebeca Yun <rebecajhyun@gmail.com>
2024-04-01 14:58:47 -0300

    new

--- 02-Aprofundando-em-Hooks/02-Ignite-timer/src/App.tsx
+++ 02-Aprofundando-em-Hooks/02-Ignite-timer/src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
