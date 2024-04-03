9550e95e9e68ec8770f30919d2409dd632c916ca
Rebeca Yun <rebecajhyun@gmail.com>
2024-04-03 17:14:49 -0300

    error

--- 02-Aprofundando-em-Hooks/02-Ignite-timer/src/pages/Home/index.tsx
+++ 02-Aprofundando-em-Hooks/02-Ignite-timer/src/pages/Home/index.tsx
@@ -11,6 +11,7 @@ import {
   Separator,
   StartCountdownButton,
   TaskInput } from "./styles";
+import { useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Name your task'),
@@ -20,14 +21,21 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'Maximum 60 minutes')
 })
 
-// interface NewCycleFormData {
-//   task: string
-//   minutesAmount: number
-// }
-
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const {cycles, setCycles} = useState([])
+
+  const {activeCycleId, setActiveCycleId} = useState<string | null>(null)
+
+  const {amountSecondsPassed, setAmountSecondsPassed} = useState(0)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -36,9 +44,30 @@ export function Home() {
     }
   })
 
-  function handleCreateNewCycle(data) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount
+    }
+
+    setCycles([...cycles, newCycle])
+    setActiveCycleId(id)
     reset();
   }
+
+  // const activeCycle = cycles.find((cycle) => cycle.id == activeCycle.id)
+
+  // const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  // const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  // const minutesAmount = Math.floor(currentSeconds / 60)
+  // const secondsAmount = currentSeconds / 60
+
+  // const minutes = String(minutesAmount).padStart(2, '0')
+  // const seconds = String(secondsAmount).padStart(2, '0')
 
   const task = watch('task')
   const isSubmitDisabled = !task
