a841f1f66bf9ef5941643a93cbb150b807744264
Rebeca Yun <rebecajhyun@gmail.com>
2024-04-18 17:36:23 -0300

    setup

--- 04-Integrando-Frontend-Backend/src/App.tsx
+++ 04-Integrando-Frontend-Backend/src/App.tsx
@@ -1,14 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import './global.css'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate='%s | pizza.shop'/>
-      
+
       <RouterProvider router={router} />
     </HelmetProvider>
   )
